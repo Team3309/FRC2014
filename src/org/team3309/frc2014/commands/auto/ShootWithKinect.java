14d5c19904853e018a51ee96d85099f302b1ac30
Vinnie Magro <vmagro@socaldevs.com>
2014-03-29 19:09:48 -0700

    ShootWithKinect actually works

--- src/org/team3309/frc2014/commands/auto/ShootWithKinect.java
+++ src/org/team3309/frc2014/commands/auto/ShootWithKinect.java
@@ -27,6 +27,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Kinect;
 import edu.wpi.first.wpilibj.Skeleton;
 import edu.wpi.first.wpilibj.command.Command;
+import org.team3309.frc2014.commands.catapult.Shoot;
 import org.team3309.frc2014.subsystems.Catapult;
 
 /**
@@ -34,7 +35,7 @@ import org.team3309.frc2014.subsystems.Catapult;
  */
 public class ShootWithKinect extends Command {
 
-    private static final double ANGLE_THRESH = -.7;
+    private static final double ANGLE_THRESH = .7;
 
     private boolean finished = false;
 
@@ -54,10 +55,16 @@ public class ShootWithKinect extends Command {
         Skeleton skeleton = kinect.getSkeleton();
         Skeleton.Joint leftHand = skeleton.GetHandLeft();
         Skeleton.Joint rightHand = skeleton.GetHandRight();
+        Skeleton.Joint center = skeleton.GetHipCenter();
+
+        double left = (leftHand.getY() + 1) - (center.getY() + 1);
+        double right = (rightHand.getY() + 1) - (center.getY() + 1);
+
+        System.out.println("left:" + left + "\tright:" + right);
 
         //this will require the drive to raise their hands roughly vertical to get it to shoot
-        if (leftHand.getY() < ANGLE_THRESH && rightHand.getY() < ANGLE_THRESH) {
-            Catapult.getInstance().unlatch();
+        if (left > ANGLE_THRESH && right > ANGLE_THRESH) {
+            new Shoot().start();
             finished = true;
         }
     }
