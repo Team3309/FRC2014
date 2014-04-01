e5b6288ec8983a360db999a707ed0cd23c0717dd
Vinnie Magro <vmagro@socaldevs.com>
2014-04-01 14:04:33 -0700

    KinectRunningAuto mode

---
+++ src/org/team3309/frc2014/commands/auto/KinectRunningAuto.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2014. FRC Team 3309 All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.team3309.frc2014.commands.auto;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Kinect;
+import edu.wpi.first.wpilibj.Skeleton;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import org.team3309.frc2014.commands.catapult.ShootAndRetract;
+import org.team3309.frc2014.subsystems.Drive;
+
+/**
+ * Created by vmagro on 4/1/14.
+ */
+public class KinectRunningAuto extends Command {
+
+    private static final int STATE_WAITING = 0;
+    private static final int STATE_SHOOTING = 1;
+    private static final int STATE_DONE = 2;
+
+    private Drive drive;
+    private Kinect kinect;
+
+    private int state = 0;
+    private Timer stateTimer = new Timer();
+
+    public KinectRunningAuto() {
+        drive = Drive.getInstance();
+        requires(drive);
+
+        kinect = Kinect.getInstance();
+    }
+
+    protected void initialize() {
+        stateTimer.start();
+    }
+
+    protected void execute() {
+        if (state == STATE_WAITING) {
+            Skeleton skeleton = kinect.getSkeleton();
+            Skeleton.Joint leftHand = skeleton.GetHandLeft();
+            Skeleton.Joint rightHand = skeleton.GetHandRight();
+            Skeleton.Joint head = skeleton.GetHead();
+
+            //this will require the drive to raise their hands roughly vertical to get it to shoot or timeout after 5 seconds
+            if (leftHand.getY() > head.getY() && rightHand.getY() > head.getY() || stateTimer.get() > 5) {
+                drive.driveTank(1, 0);
+                Timer.delay(1.5);
+                new ShootAndRetract().start();
+                state = STATE_DONE;
+            }
+        } else if (state == STATE_DONE) {
+            drive.driveTank(0, 0);
+        }
+    }
+
+    protected boolean isFinished() {
+        return !DriverStation.getInstance().isAutonomous();
+    }
+
+    protected void end() {
+
+    }
+
+    protected void interrupted() {
+
+    }
+}
